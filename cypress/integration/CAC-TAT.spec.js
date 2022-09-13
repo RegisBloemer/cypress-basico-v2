9effc3062bda1cd91dc5e75300c218655c523492
Régis Nyland Bloemer <regisnb1@gmail.com>
2022-09-13 11:29:06 -0300

    alguns exercicios finalizados

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,63 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+
+    beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal',"Central de Atendimento ao Cliente TAT")
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        
+        const text = 'ESta mensagem é apenas para teste. ESta mensagem é apenas para teste. ESta mensagem é apenas para teste. ESta mensagem é apenas para teste.'
+
+        cy.get('#firstName').type('Régis')
+        cy.get('#lastName').type('Nyland Bloemer')
+        cy.get('#email').type('regis@teste.com')
+        cy.get('#open-text-area').type(text, {delay: 0})
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+
+        cy.get('#firstName').type('Régis')
+        cy.get('#lastName').type('Nyland Bloemer')
+        cy.get('#email').type('registeste.com')
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('verificação númerica', function(){
+
+        cy.get('#firstName').type('Régis')
+        cy.get('#lastName').type('Nyland Bloemer')
+        cy.get('#email').type('regis@teste.com')
+        cy.get('#open-text-area').type('Esta mensagem é apenas para teste')
+        cy.get('#phone').type('asjbadbauf').should('have.value', '')
+        cy.get('button[type="submit"]').click()
+
+    })
+
+
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){    
+        cy.get('#firstName').type('Régis')
+        cy.get('#lastName').type('Nyland Bloemer')
+        cy.get('#email').type('regis@teste.com')
+        cy.get('#phone-checkbox')
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName').type('Régis').should('have.value', 'Régis')
+        cy.get('#lastName').type('Nyland Bloemer').should('have.value', 'Nyland Bloemer')
+        cy.get('#email').type('regis@teste.com').should(have)
+        cy.get('#phone').type('8994893899').should('have.value', '8994893899')
+    })
+  })
+  
